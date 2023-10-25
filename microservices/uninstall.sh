751e8e8469f25605985d0acdbd9d28519ffb9725
Christoph Heider <christoph.heider@vema-eg.de>
2023-10-25 08:14:05 +0200

    Demo

---
+++ microservices/uninstall.sh
@@ -0,0 +1,12 @@
+helm uninstall rediscart
+
+helm uninstall adservice
+helm uninstall cartservice
+helm uninstall checkoutservice
+helm uninstall currencyservice
+helm uninstall emailservice
+helm uninstall frontend
+helm uninstall paymentservice
+helm uninstall productcatalogservice
+helm uninstall recommendationservice
+helm uninstall shippingservice
